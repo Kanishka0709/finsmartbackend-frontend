153c1c0a29a5a59b55d10a1a9bfa5ea727a8935d
Kanishka0709 <kanishkaamrutkar0709@gmail.com>
2026-04-29 22:54:28 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-pkill -f "Finsmart_Finances" || true
-sleep 2
-nohup java -jar /opt/finsmart/Finsmart_Finances-0.0.1-SNAPSHOT.jar \
-  --spring.datasource.url=$(aws secretsmanager get-secret-value \
-    --secret-id finsmart/app-config --query 'SecretString' \
-    --output text | jq -r '."spring.datasource.url"') \
-  > /opt/finsmart/app.log 2>&1 &
+
+echo "Stopping old app..."
+pkill -f Finsmart_Finances || true
+sleep 3
+
+echo "Starting new app..."
+
+cd /opt/finsmart
+
+nohup java -jar Finsmart_Finances-0.0.1-SNAPSHOT.jar > app.log 2>&1 &
